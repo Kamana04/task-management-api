b8dc968e97f649893523c5fae6b22f71ca3fbb2b
kamana1997 <104790201+kamana1997@users.noreply.github.com>
2026-07-03 12:06:28 +0530

    mandate fields validation

--- src/main/java/com/example/task_management_api/dto/TaskRequest.java
+++ src/main/java/com/example/task_management_api/dto/TaskRequest.java
@@ -1,6 +1,9 @@
 package com.example.task_management_api.dto;
 
 import com.example.task_management_api.domain.TaskStatus;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,11 +11,14 @@ import java.time.LocalDate;
 @Data
 public class TaskRequest {
 
+    @NotBlank(message = "Title is mandatory")
     private String title;
 
     private String description;
 
     private TaskStatus taskStatus = TaskStatus.PENDING;
 
+    @NotNull(message = "Due date is mandatory")
+    @FutureOrPresent(message = "Due date must be in the future or present")
     private LocalDate dueDate;
 }
